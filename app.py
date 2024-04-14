2a1c32a6edf241524bebf75e4b449580d72a64e6
MrJango0 <105359208+MrJango0@users.noreply.github.com>
2024-04-14 18:14:05 +0200

    Added darkmode, the real names of the countries, text autocomplete for the guess

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import random
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
     # Pick a random flag
@@ -33,4 +31,4 @@ def check_guess():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=80)
