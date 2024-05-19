d618363099ca1134823584c300cd056ccda02c9d
MrJango0 <105359208+MrJango0@users.noreply.github.com>
2024-05-19 13:38:44 +0200

    fixed a small bug

--- create_db.py
+++ create_db.py
@@ -6,13 +6,14 @@ from config import salt
 with app.app_context():
     db.create_all()
 
-    user = models.User("Test", hashlib.pbkdf2_hmac('sha256', "Test".encode('utf-8'), salt, 100000))
-    db.session.add(user)
+    with db.session.begin():
+        db.session.query(models.User).delete()
+        db.session.query(models.Highscore).delete()
+        db.session.query(models.Flag).delete()
     db.session.commit()
 
-    with db.session.begin():
-        db.session.query(models.Flag).delete()
-
+    user = models.User("Test", hashlib.pbkdf2_hmac('sha256', "Test".encode('utf-8'), salt, 100000))
+    db.session.add(user)
     db.session.commit()
 
     for flag in FLAGS:
