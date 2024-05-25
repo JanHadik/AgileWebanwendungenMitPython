5ef6e4301f499b66e6306dea06dc969120ca9607
MrJango0 <105359208+MrJango0@users.noreply.github.com>
2024-05-25 12:20:01 +0200

    added navbar added login/logout functionality refactored render_game()

--- Flagstravaganza/views.py
+++ Flagstravaganza/views.py
@@ -1,35 +1,45 @@
+import hashlib
 import random
 
-from flask import render_template, request, jsonify, redirect, url_for
-from instance.flag_list import FLAGS
+from flask import render_template, request, jsonify, redirect, url_for, session
 from Flagstravaganza import app, db
-from Flagstravaganza.models import User
+from Flagstravaganza.models import User, Flag
+from config import salt
 
-
-
-# Extract country names from FLAGS list
-countries = [flag['country'] for flag in FLAGS]
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
 
-    user = User.query.filter_by(username=username, password=password).first()
+    hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+    user = User.query.filter_by(username=username, password=hashed_password).first()
     if user:
         # Redirect to another HTML page on successful login
-        return redirect(url_for('game'))
+        session["logged_in"] = True
+        return render_game()
     else:
-        return 'Invalid username or password. Please try again.'
+        return render_game()
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session["logged_in"] = False
+    return render_game()
+
 
 @app.route('/game')
 def game():
-    flag = random.choice(FLAGS)
-    return render_template('game.html', flag=flag, countries=countries)
+    if session.get("logged_in") is None:
+        session["logged_in"] = False
+    return render_game()
+
 
 @app.route('/check_guess', methods=['POST'])
 def check_guess():
@@ -42,9 +52,25 @@ def check_guess():
     # Check if the guess is correct
     if guessed_country == flag_country:
         # If correct, pick a new random flag
-        new_flag = random.choice(FLAGS)
+        flags = Flag.query.all()
+        new_flag = random.choice(flags)
+        new_flag_data = {"img_path": new_flag.img_path, "country": new_flag.country}
         result = "Correct! Here's a new flag."
-        return jsonify(result=result, new_flag=new_flag)
+        return jsonify(result=result, new_flag=new_flag_data)
     else:
         result = "Incorrect! Try again."
         return jsonify(result=result)
+
+
+def render_game():
+    # Retrieve all flags and countries from the database
+    flags = Flag.query.all()
+    countries = [flag.country for flag in flags]
+    flag = random.choice(flags)
+    flag_data = {"img_path": flag.img_path, "country": flag.country}
+
+    return render_template('game.html', flag=flag_data, countries=countries, logged_in=session["logged_in"])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
